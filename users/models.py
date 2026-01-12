71d1c1271591222fd110d75a113c8bcf40e514f7
Dasakami <dendasakami@gmail.com>
2026-01-12 19:00:50 +0600

    temporary

--- users/models.py
+++ users/models.py
@@ -12,22 +12,6 @@ class User(AbstractUser):
         blank=True,
         unique=True
     )
-    groups = models.ManyToManyField(
-        Group,
-        related_name="custom_user_set",
-        blank=True,
-        help_text="The groups this user belongs to.",
-        verbose_name="groups",
-    )
-
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name="custom_user_set",
-        blank=True,
-        help_text="Specific permissions for this user.",
-        verbose_name="user permissions",
-    )
-
 
 class CharacterClass(models.Model):
     name = models.CharField(max_length=100)
