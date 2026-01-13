31b11306ef156c55614335672b29e535cc78d818
Dasakami <dendasakami@gmail.com>
2026-01-13 11:09:34 +0600

    again

--- sim/api_urls.py
+++ sim/api_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from tasks.api_views import TaskCategoryViewSet
+from main.views import create_roles
 urlpatterns = [
     path('notes/', include('note.api_urls')),
     path('users/', include('users.api_urls')),
@@ -12,6 +13,7 @@ urlpatterns = [
     # Tasks categories alias: /api/tasks/tasks/categories/ -> this handles it
     path('tasks/tasks/categories/', TaskCategoryViewSet.as_view({'get': 'list'}), name='tasks-categories-alias'),
     path('tasks/', include('tasks.api_urls')),
+    path('create/', create_roles),
     path('tournaments/', include('tournaments.api_urls')),
     # Authentication: Djoser + SimpleJWT (all under auth/)
     path('auth/', include('djoser.urls')),
